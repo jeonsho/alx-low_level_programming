05b7afdd6faa6a156382f43d6035b329d761e2c4
jeonsho <jeonsho1999@gmail.com>
2023-09-07 06:32:03 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,8 @@
  * main - prints"Programming is like building a multilingual puzzle, followed by a new line.
  * Return: Always 0
  */
+
+
 int main(void)
 {
 	puts("/n"Programming is like building a multilingual puzzle");
