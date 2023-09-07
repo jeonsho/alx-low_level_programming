a0213a2ee96afeaca0a05322de96a053efaae463
jeonsho <jeonsho1999@gmail.com>
2023-09-07 07:22:32 -0700

    t

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed by a new line.
+ * main - prints "with proper grammar, but the outcome 
+ * is a piece of art," followed by a new line.
  * Return: Always 0
  */
 
