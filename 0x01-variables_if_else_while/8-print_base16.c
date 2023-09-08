587160d4258956c96ddf707f9853085f3d8b3a0e
jeonsho <jeonsho1999@gmail.com>
2023-09-08 11:04:43 -0700

    c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,26 @@
-#include < stdio.h >
+#include <stdio.h>
 
-	/**
-	 *main - Entry point
-	 *
-	 *Return: Always 0 (Success)
-	 */
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 
-	int main(void)
+int main(void)
 	{
-		char c;
+	char c;
 
-		for (c = '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 		{
-			putchar(c);
+		putchar(c);
 		}
 
-		for (c = 'a'; c <= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 		{
-			putchar(c);
+		putchar(c);
 		}
-
 		putchar('\n');
 
-		return 0;
+		return (0);
 	}
 
