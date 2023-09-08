6e3aa720f87741e7bf80e987ec8a88c46b661bc9
jeonsho <jeonsho1999@gmail.com>
2023-09-08 11:25:11 -0700

    c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,26 @@
  */
 
 int main(void)
-{   int i,j;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i + 1; j < 10; j++) 
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-            putchar('0' + i);
-            putchar('0' + j);
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
 
-            if (i < 8 || j < 9)
-	    {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    return (0);
+	putchar('\n');
+
+	return (0);
 }
 
