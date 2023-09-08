896d6ed97f2381d416d952a5de9b4aac03971132
jeonsho <jeonsho1999@gmail.com>
2023-09-08 11:36:10 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,35 +3,34 @@
 /**
  *main - Entry point 
  *
- * 
+ *
  *Return: Always 0(Success)
  */
 
- int main(void)
- {
- 	int i, j, k;
+int main(void)
+{
+	int i, j, k;
 
- 	for (i = 0; i < 10; i++)
- 	{
- 		for (j = i + 1; j < 10; j++)
- 		{
- 			for (k = j + 1; k < 10; k++)
- 			{
- 				putchar('0' + i);
- 				putchar('0' + j);
- 				putchar('0' + k);
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
 
- 				if (i < 7 || j < 8 || k < 9)
- 				{
- 					putchar(',');
- 					putchar(' ');
- 				}
- 			}
- 		}
- 	}
+				if (i < 7 || j < 8 || k < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
- 	putchar('\n');
-
- 	return (0);
- }
+	putchar('\n');
+	return (0);
+}
 
