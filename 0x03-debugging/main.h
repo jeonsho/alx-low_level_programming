dfa28f45270d13907f7e26077f2862f76038bec3
jeonsho <jeonsho1999@gmail.com>
2023-09-13 10:03:06 -0700

    b

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -25,4 +25,12 @@ void print_remaining_days(int month, int day, int year);
 * @year: year
 * Return: void
 */
+int convert_day(int month, int day);
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
 #endif /* MAIN_H */
