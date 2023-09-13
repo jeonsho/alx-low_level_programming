18fb7d7149f78fc2a1a4345e34fcca8324f26ce6
jeonsho <jeonsho1999@gmail.com>
2023-09-13 09:08:32 -0700

    b

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,10 @@ void positive_or_negative(int i);
 * Return: 0
 */
 int largest_number(int a ,int b,int c);
-
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
