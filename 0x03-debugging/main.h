3ba38dfa6d08b158d4038656b4d062130418f51f
jeonsho <jeonsho1999@gmail.com>
2023-09-13 08:39:06 -0700

    b

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,4 +8,9 @@ void positive_or_negative(int i);
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+int largest_number(int ,int ,int );
 
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
