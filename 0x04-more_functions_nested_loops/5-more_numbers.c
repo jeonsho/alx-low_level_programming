e57413ebfcd8c2fb5b57d5a2e0616c0d68fc5b19
jeonsho <jeonsho1999@gmail.com>
2023-09-14 05:19:06 -0700

    v

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,10 @@ void more_numbers(void)
 			{
 				if (j > 9)
 				{
-					_putchar(j / 10 + '0');
+					_putchar((j / 10) + '0');
 				}
 
-				_putchar(j % 10 + '0');
+				_putchar((j % 10) + '0');
 
 				_putchar(10);
 			}
