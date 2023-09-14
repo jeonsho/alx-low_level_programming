ccc08df33a1cc84dfaf5c42e51d7776437c2c596
jeonsho <jeonsho1999@gmail.com>
2023-09-14 04:06:40 -0700

    v

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,11 +2,15 @@
 #define MAIN_H
 
 /**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-#endif /* MAIN_H */
+
+
+
+#endif /*MAIN_H */
