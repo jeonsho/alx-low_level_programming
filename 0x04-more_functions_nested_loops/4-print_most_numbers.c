7a450d87167b87f0d5b2ef233d8a09123070bfb2
jeonsho <jeonsho1999@gmail.com>
2023-09-14 04:34:25 -0700

    v

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print the  the numbers, from 0 to 9
+ *followed by a new line.
+ *Return: returns nothing
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		if (num != 50 && num != 52)
+		{
+			continue;
+		}
+
+		_putchar(num);
+	}
+
+	_putchar(10);
+}
