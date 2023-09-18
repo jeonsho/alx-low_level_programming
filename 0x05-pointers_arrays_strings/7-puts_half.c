a2077bcc77b124acff7001c22a704f19470b0ba7
jeonsho <jeonsho1999@gmail.com>
2023-09-18 10:33:17 -0700

    g

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,30 @@
 #include "main.h"
 
 /**
- * puts_half -  prints half of a string
- *  followed by a new line.
- * @str: string to be printed
+ *puts_half -  prints half of a string
+ *followed by a new line.
+ *@str: string to be printed
  */
 void puts_half(char *str)
 {
-	int len ,st_index,i;
-	
-	len=0;	
+	int len, st_index, i;
+
+	len = 0;
 	while (str[len] != '\0')
 	{
 		len++;
 	}
+
 	if (len % 2 == 0)
 	{
-	 	for (i = len/2 ; str[i] != '\0' ; i++)
+		for (i = len / 2; str[i] != '\0'; i++)
 			_putchar(str[i]);
 	}
 	else if (len % 2)
 	{
-		for (st_index = (len - 1) / 2 ;st_index < (len - 1); st_index++)
+		for (st_index = (len - 1) / 2; st_index < (len - 1); st_index++)
 			_putchar(str[st_index + 1]);
 	}
+
 	_putchar('\n');
 }
