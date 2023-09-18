aeaaa641acaf46150f9fd9a76a375aa26985a065
jeonsho <jeonsho1999@gmail.com>
2023-09-18 08:46:03 -0700

    v

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - prints one char out of 2 of a string
+ *followed by a new line
+ *@str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}	
+	
