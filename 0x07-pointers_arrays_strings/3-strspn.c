bff7902285fd8790f008085eb054281cc44e1635
jeonsho <jeonsho1999@gmail.com>
2023-09-25 08:07:01 -0700

    f

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,10 +14,9 @@ unsigned int _strspn(char *s, char *accept)
 	if (*accept == '\0' || *s == '\0')
 		return (0);
 	while (s[j] != '\0')
-	{	
-		i = 0;
+	{
 		m = 0;
-		for (; accept[i] != '\0'; i++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
 			if (s[j] == accept[i])
 			{
