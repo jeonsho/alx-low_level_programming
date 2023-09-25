c9bca292be59a387c88e4c3c7f4d3c8715c0cf9b
jeonsho <jeonsho1999@gmail.com>
2023-09-25 04:02:06 -0700

    v

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,9 +2,10 @@
 
 /**
  * _memcpy - a function that copies memory area
- * @dest: A pointer to the destination memory area where the data will be copied.
- * @src: A pointer to the source memory area from which data will be copied.
+ * @dest: A pointer to the destination memory area
+ * @src: A pointer to the source memory area.
  * @n: The number of bytes to copy from src to dest
+ * Return:  a pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
