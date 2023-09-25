6379a4741b891e12d77ba3cbd61456a8734d6de1
jeonsho <jeonsho1999@gmail.com>
2023-09-25 08:59:15 -0700

    f

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,10 +8,9 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
 	int j;
 
-	while (s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 			if (s[i] == accept[j])
