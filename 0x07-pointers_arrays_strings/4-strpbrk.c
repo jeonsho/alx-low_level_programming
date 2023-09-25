72c7b2f8f682f264e4448ec5ae46cab9428f96d2
jeonsho <jeonsho1999@gmail.com>
2023-09-25 08:55:10 -0700

    f

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes..
+ *@s: first value -char
+ *@accept: second value - char
+ *
+ * Return: char with result
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+	}
+
+	s++;
+	return (NULL);
+}
+	
+		
+	
+
