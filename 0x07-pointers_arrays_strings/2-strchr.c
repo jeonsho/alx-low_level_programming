71ad4d862aae41b39de87237497369d9afe37b13
jeonsho <jeonsho1999@gmail.com>
2023-09-25 04:42:29 -0700

    v

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,14 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		s++;
+		i++;
 	}
 
 	return (NULL);
