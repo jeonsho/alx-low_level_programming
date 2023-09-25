e039f86b96417066992471dfa79fede2d1ac197a
jeonsho <jeonsho1999@gmail.com>
2023-09-25 08:23:41 -0700

    f

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _strchr - a function that locates a character in a string
- * @s: A pointer to the string in which we want to locate the c
- * @c:The character we want to locate in the string s
- * Return: s or null
+ *_strchr - function that locates a character in a string
+ *@s: A pointer to the string
+ *@c: The character we want to locate
+ *
+ *Return: char with result
  */
-
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	i = 0;
 	while (s[i] != c && s[i] != '\0')
 	{
 		i++;
 	}
+
 	if (s[i] == c)
 		return (s + i);
 	else
