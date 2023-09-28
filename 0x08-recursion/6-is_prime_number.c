617b63fd0c208c06b3fc4c937436d013e317e74a
jeonsho <jeonsho1999@gmail.com>
2023-09-28 07:38:24 -0700

    v

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@
  *is_prime_number - a function that returns 1 if the input integer is a
  *prime number, otherwise return 0.
  *
- *@prime: prime number
+ *@n: prime number
  *
  *Return: Always succes
  */
