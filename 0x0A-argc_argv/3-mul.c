0402f87d254d714fcca81c2e029a2e91b211431c
jeonsho <jeonsho1999@gmail.com>
2023-10-02 08:41:01 -0700

    v

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints multiplication
+*of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j, mul;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = all(argv[1]);
+	j = all(argv[2]);
+	mul = i * j;
+
+	printf("%d\n", mul);
+	return (0);
+}
