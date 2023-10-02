44f000171388b6d1b1ed38e8925181dcad1e6d8b
jeonsho <jeonsho1999@gmail.com>
2023-10-02 08:00:03 -0700

    v

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
 * main - prints the number of arguments
 *@argc: number of arguments
 *@argv:array of arguments
-*Return: the number of arguments
+*Return: 0
 */
 int main(int argc, char *argv[])
 {
-	(void)argc;
 
 	printf("%d\n", argc - 1);
 	return(0);
