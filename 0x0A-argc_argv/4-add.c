e9d28991224c6fb4f97f861031287c0ea0e05662
jeonsho <jeonsho1999@gmail.com>
2023-10-02 09:44:35 -0700

    v

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *main - Entry point of the program
+ *@argc: Number of command-line arguments
+ *@argv: Array of command-line argument strings
+ *Return:
+ *  - 0: The program executed successfully.
+ *  - 1: An error occurred, such as non-digit input or negative numbers.
+ */
 int main(int argc, char *argv[])
 {
 	int sum = 0, int i;
@@ -14,7 +22,9 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 			char *endptr;
+
 			long num = strtol(argv[i], &endptr, 10);
+
 			if (*endptr != '\0' || num < 0)
 			{
 				printf("Error\n");
