c0dddaa8724d8d3838ab193e914c19065265f08f
jeonsho <jeonsho1999@gmail.com>
2023-10-02 08:50:30 -0700

    v

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - adds positive numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0 on success, 1 on failure
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		if (num <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
