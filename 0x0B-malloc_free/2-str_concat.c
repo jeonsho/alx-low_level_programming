c130bb8af8f388eafcf800fa5582cb115724dbaf
jeonsho <jeonsho1999@gmail.com>
2023-10-04 13:38:18 -0700

    v

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,6 @@
  *str_concat - concatenates two strings
  *@s1: first input string
  *@s2: second input string
- *
  *Return: returns NULL on failer
  */
 char *str_concat(char *s1, char *s2)
@@ -19,41 +18,26 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-
 	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
-
 	str = (char *) malloc(len1 + len2 + 1);
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-
 	for (i = 0; i < len1; i++)
-	{
 		str[i] = s1[i];
-	}
 	for (i = 0; i < len2; i++)
-	{
 		str[len1 + i] = s2[i];
-	}
-
 	str[len1 + len2 + 1] = '\0';
-
 	return (str);
 }
-
-
