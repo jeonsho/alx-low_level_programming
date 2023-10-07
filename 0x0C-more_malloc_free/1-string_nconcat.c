eb6ae29771c9e38545949ee2ffac50f02dcefdac
jeonsho <jeonsho1999@gmail.com>
2023-10-07 04:53:31 -0700

    v

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,22 +21,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		j++;
 	}
-
 	concat = malloc(sizeof(char) * (j + 1));
-
+	
+	if (concat == NULL)
+		return (NULL);
 	j = 0;
 
 	for (i = 0; s1[i]; i++)
 	{
 		concat[j++] = s1[i];
 	}
-
 	for (i = 0; s2[i] && i < n; i++)
 	{
 		concat[j++] = s2[i];
 	}
-
 	concat[j] = '\0';
-
 	return (concat);
 }
