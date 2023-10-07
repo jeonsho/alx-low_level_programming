f67cd3999fafaff0a33f8e8e2739d5e43e75a64a
jeonsho <jeonsho1999@gmail.com>
2023-10-07 06:21:48 -0700

    v

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *_calloc -  allocates memory for an array
+ *@nmemb: number of elements
+ *@size: size of the memory block to be allocated
+ *Return: poiner to the address of the memory block
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *block;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	block = malloc(nmemb *size);
+
+	if (block == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb *size); i++)
+	{
+		block[i] = 0;
+	}
+
+	return (block);
+}
+
