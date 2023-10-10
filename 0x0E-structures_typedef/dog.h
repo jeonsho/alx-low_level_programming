1e8af3eeb038bf2bbc764abc045a0ce69054533e
jeonsho <jeonsho1999@gmail.com>
2023-10-10 06:52:47 -0700

    v

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,6 @@ char *name;
 float age;
 char *owner;
 };
-/**
- * my_dog -  Typedef for struct dog
- */
-typedef struct dog my_dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
