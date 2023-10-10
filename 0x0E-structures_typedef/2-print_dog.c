d25bcea372581e9348d5049a74f8512021c87f02
jeonsho <jeonsho1999@gmail.com>
2023-10-10 06:49:54 -0700

    v

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,8 +8,8 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if ((*d).name == NULL)
-		printf("nil\n");
+	if ((*d).name == '\0')
+		printf("Name: nil\n");
 	else
 	{
 		printf("Name: %s\n", d->name);
@@ -17,8 +17,8 @@ void print_dog(struct dog *d)
 
 	printf("Age: %f\n", d->age);
 
-	if ((*d).owner == NULL)
-		printf("nil\n");
+	if ((*d).owner == '\0')
+		printf("Owner: nil\n");
 	else
 	{
 		printf("Owner: %s\n", d->owner);
