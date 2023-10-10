34f90029446ae32feee3c9667ea2b81f3cafcf5d
jeonsho <jeonsho1999@gmail.com>
2023-10-10 05:53:23 -0700

    v

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
 #include "dog.h"
 
 /**
-* init_dog - inits a variable of
-*type dog
-*@d: pointer to the adress to initialize
-*@name: name of dog
-*@age: age of dog
-*@owner: owner's name
-*/
+ *init_dog - inits a variable of
+ *type dog
+ *@d: pointer to the adress to initialize
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner's name
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
