d0f580dc2e97753eb1998ec88cb495ba9859e749
jeonsho <jeonsho1999@gmail.com>
2023-10-11 02:07:32 -0700

    v

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,9 +15,7 @@ void array_iterator(int *array, int size, void(*action)(int))
 
 	if (arry == NULL || action == NULL)
 		return;
-	else
-	{
-		for (a = 0; a < size; a++)
-			action(array[a]);
-	}
+
+	for (a = 0; a < size; a++)
+	action(array[a]);
 }
