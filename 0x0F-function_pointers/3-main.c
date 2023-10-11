40a459c1f149660719d7c7682db0f1119899316c
jeonsho <jeonsho1999@gmail.com>
2023-10-11 03:21:58 -0700

    v

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,29 +1,41 @@
 #include "3-calc.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - a basic calculator
- * @argc: the size of the argument vector
- * @argv: the argument vector
- * Return: Always 0
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	int (*f)(int, int);
+	int a, b;
+	int (*o)(int, int);
 
-	if (f == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	f = get_op_func(argv[2]);
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	o = get_op_func(argv[2]);
+	if (o == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", o(a, b));
 
 	return (0);
 }
