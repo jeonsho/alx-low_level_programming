f85a11dc51923635c1f20389b848259bcc408e7f
jeonsho <jeonsho1999@gmail.com>
2023-10-10 22:50:56 -0700

    v

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - a basic calculator
+ * @argc: the size of the argument vector
+ * @argv: the argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (argc != 4)
+	{
+		printf("Error");
+		exit(98);
+	}
+	f = get_op_func(argv[2]);
+
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
