969dd6ac7d4b65821178b987737aaaab0dafb30d
jeonsho <jeonsho1999@gmail.com>
2023-10-12 10:38:14 -0700

    v

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,10 +6,6 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void op_c(va_list var);
-void op_i(va_list var);
-void op_f(va_list var);
-void op_s(va_list var);
 /**
  * struct format - Data type of a format.
  * @op: Format.
