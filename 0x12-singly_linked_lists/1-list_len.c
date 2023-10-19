932cdbd16c59435cb6a33dae5a3fc722be3683e0
jeonsho <jeonsho1999@gmail.com>
2023-10-19 05:00:22 -0700

    test

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ *list_len - Returns the number of elements in a list_t list.
+ *@h: The list_t list.
+ *Return: The number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return count;
+}
