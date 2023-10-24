a784ea945d2520f431a243d6e1488922d90ab739
jeonsho <jeonsho1999@gmail.com>
2023-10-24 03:13:19 -0700

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,6 @@
 /**
  *print_listint - prints all the elements of a linked list
  *@h: A pointer to the head of the linked list.
- *
  *Return: The number of nodes in the linked list.
  */
 size_t print_listint(const listint_t *h)
