5435be94a54ce560047f64597fee3a9397bb1d71
jeonsho <jeonsho1999@gmail.com>
2023-10-26 05:47:27 -0700

    test

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *get_bit - Gets the value of a bit at a given index.
+ *@n: The unsigned long int.
+ *@index: The index of the bit to retrieve (starting from 0).
+ *
+ *Return: The value of the bit at the given i
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int maxindex = sizeof(n) * 8 - 1;
+
+	if (index > maxindex)
+	{
+		return (-1);
+	}
+	return ((n >> index) & 1);
+}
