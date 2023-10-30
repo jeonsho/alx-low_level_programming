b087b93082fe5f7b11b0b0967bd2187dc7011f9f
jeonsho <jeonsho1999@gmail.com>
2023-10-30 11:46:30 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,9 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int input_file, output_file;
-	int bytes_read, bytes_written;
 	char buffer[1024];
+	int bytes_read, input_file, output_file, bytes_written;
 
 	if (argc != 3)
 	{
@@ -22,15 +21,15 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
-		S_IRGRP | S_IWGRP | S_IROTH);
+	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (output_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(input_file);
 		exit(99);
 	}
-	while ((bytes_read = read(input_file, buffer, 1024)) > 0)
+	while ((bytes_read = read(input_file, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(output_file, buffer, bytes_read);
 		if (bytes_written < bytes_read)
@@ -39,15 +38,12 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	if (close(input_file) == -1)
+	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", input_file);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	if (close(output_file) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", output_file);
-		exit(100);
-	}
+	close(input_file);
+	close(output_file);
 	return (0);
 }
