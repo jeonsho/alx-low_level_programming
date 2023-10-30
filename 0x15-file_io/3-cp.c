e8510fefebd3c6804100086bc30c1558efb22202
jeonsho <jeonsho1999@gmail.com>
2023-10-30 12:40:06 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,14 @@
 #include "main.h"
-#define BUFFER_SIZE 1024
 /**
- *main - Program that copies the content 
- *@argc: The number of command-line arguments.
- *@argv: An array of strings
- *Return: 0 on success.
+ *main - program that copies the content of a file to another file
+ *@argc: The number of command-line arguments
+ *@argv: string argument
+ *Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int input_file, output_file, bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	char buffer[1024];
+	int bytes_read, input_file, output_file, bytes_written;
 
 	if (argc != 3)
 	{
@@ -22,10 +21,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (output_file == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(input_file);
 		exit(99);
 	}
 	while ((bytes_read = read(input_file, buffer, sizeof(buffer))) > 0)
@@ -33,7 +34,7 @@ int main(int argc, char *argv[])
 		bytes_written = write(output_file, buffer, bytes_read);
 		if (bytes_written < bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
@@ -42,10 +43,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(input_file) == -1 || close(output_file) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-		exit(100);
-	}
+	close(input_file);
+	close(output_file);
 	return (0);
 }
