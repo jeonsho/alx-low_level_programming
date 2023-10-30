bf30392316ba875471b2d83a50bb110d088d9622
jeonsho <jeonsho1999@gmail.com>
2023-10-30 13:23:08 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,12 +22,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (output_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(input_file);
 		exit(99);
 	}
 	while ((bytes_read = read(input_file, buffer, sizeof(buffer))) > 0)
@@ -39,21 +37,12 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(input_file) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", input_file);
-		exit(100);
-	}
-	if (close(output_file) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", output_file);
-		exit(100);
-	}
+	close(input_file);
+	close(output_file);
 	return (0);
 }
