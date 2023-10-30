ea0024bb16a94a7daf2d60d0132c91c3fbbb7c74
jeonsho <jeonsho1999@gmail.com>
2023-10-30 12:17:16 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	char buffer[1024];
-	int bytes_read, input_file, output_file, bytes_written;
+	int input_file, output_file, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
@@ -22,20 +22,18 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	output_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (output_file == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(input_file);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-	while ((bytes_read = read(input_file, buffer, sizeof(buffer))) != -1)
+	while ((bytes_read = read(input_file, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(output_file, buffer, bytes_read);
 		if (bytes_written < bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			exit(99);
 		}
 	}
