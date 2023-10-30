e31f37247e6fc8dc2c5c587a918713e5079c3487
jeonsho <jeonsho1999@gmail.com>
2023-10-30 07:52:52 -0700

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,49 +1,36 @@
 #include "main.h"
 
 /**
- *create_file - Creates a file with specified permissions and writes content to it.
+ *create_file - Creates a file with specified permissions
  *@filename: The name of the file to be created.
  *@text_content: The NULL-terminated string to write to the file.
  *Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int fdes, write_status;
-	ssize_t inlen = 0;
-	const char *ptr;
+	int fdes, write_status, len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+		{
+			len++;
+		}
+	}
+
 	fdes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fdes == -1)
+	write_status = write(fdes, text_content, len);
+
+	if (fdes == -1 || write_status == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content != NULL)
-	{
-		ptr = text_content;
-		while (*ptr != '\0')
-		{
-			inlen++;
-			ptr++;
-		}
-
-		write_status = write(fdes, text_content, inlen);
-		if (write_status == -1)
-		{
-			close(fdes);
-			return (-1);
-		}
-	}
-	else
-	{
-		close(fdes);
-		return (1);
-	}
 	close(fdes);
 	return (1);
 }
