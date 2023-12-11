6383c4c3488bac57b51621cfa7b4511d067aeca6
jeonsho <jeonsho1999@gmail.com>
2023-12-11 03:36:47 -0800

    test

---
+++ 0x18-dynamic_libraries/memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: A pointer to the destination memory area
+ * @src: A pointer to the source memory area.
+ * @n: The number of bytes to copy from src to dest
+ * Return:  a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n ; j++)
+	{
+		dest[j] = src[j];
+	}
+
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/strpbk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes..
+ * @s: A pointer to the string in which we want to search
+ * @accept: A pointer to a string containing the bytes
+ *
+ * Return: char or null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	if (*s == '\0' || *accept == '\0')
+		return (NULL);
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		for (j = 0; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
