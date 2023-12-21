7cd8579f539e87606bd8af65b967d5874742f40e
jeonsho <jeonsho1999@gmail.com>
2023-12-21 08:16:15 -0800

    test

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -15,7 +15,7 @@ typedef struct hash_node_s
 {
 	char *key;
 	char *value;
-	struct hash_node_s * next;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -32,7 +32,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-hash_table_t* hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
