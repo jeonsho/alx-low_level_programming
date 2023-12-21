54372bffe3469df490885039d7efb09760f82707
jeonsho <jeonsho1999@gmail.com>
2023-12-21 07:49:09 -0800

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 	unsigned long int i;
-	
+
 	if (size == 0)
 		return (NULL);
 	hash_table = malloc(sizeof(hash_table_t));
