86eae56d628459a20310aab0819cc67b871026a0
jeonsho <jeonsho1999@gmail.com>
2023-12-21 07:26:31 -0800

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,6 @@
 /**
  *hash_table_create - Creates a hash table
  *@size: Size of the array
- *
  *Return: A pointer to the newly created hash table, or NULL if an error occurs
  */
 hash_table_t *hash_table_create(unsigned long int size)
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,12 +8,13 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_value;
+	unsigned long int hash_value, index;
 
 	if (key == NULL || size == 0)
 		return (0);
 
 	hash_value = hash_djb2(key);
+	index = hash_value % size
 
-	return (hash_value % size);
+	return (index);
 }
